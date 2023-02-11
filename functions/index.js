98c52a473def0e64da8687c6810dab702037483c
Ioannis Georgiades <I.GEORGIADES@student.tudelft.nl>
2023-02-11 17:17:49 +0100

    Add login, signup and delete function

--- functions/index.js
+++ functions/index.js
@@ -33,3 +33,44 @@ exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
   // Setting an 'uppercase' field in Firestore document returns a Promise.
   return snap.ref.set({uppercase}, {merge: true});
 });
+
+exports.createUsers = functions.https.onRequest(async (req, res) => {
+  const email = req.query.email;
+  const password = req.query.password;
+
+  // Create a new user with email and password.
+  try{
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password
+    });
+    res.json({result: `User with ID: ${userRecord.uid} added.`});
+  } catch (error) {
+    res.json({result: `Error: ${error}`});
+  }
+});
+
+exports.loginUsers = functions.https.onRequest(async (req, res) => {
+  const email = req.query.email;
+  const password = req.query.password;
+
+  // Create a new user with email and password.
+  try{
+    const userRecord = await admin.auth().signInWithEmailAndPassword(email, password);
+    res.json({result: `User with ID: ${userRecord.uid} added.`});
+  } catch (error) {
+    res.json({result: `Error: ${error}`});
+  }
+});
+
+exports.deleteUsers = functions.https.onRequest(async (req, res) => {
+  const uid = req.query.uid;
+
+  // Create a new user with email and password.
+  try{
+    const userRecord = await admin.auth().deleteUser(uid);
+    res.json({result: `User with ID: ${userRecord.uid} added.`});
+  } catch (error) {
+    res.json({result: `Error: ${error}`});
+  }
+});
